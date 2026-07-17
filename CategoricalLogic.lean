870e71d1471c7cd5e56f6e45ceccd7b85510f1ee
Adrian Marti <25987878+adrianmartir@users.noreply.github.com>
2026-07-17 14:18:03 +0200

    Update

--- CategoricalLogic.lean
+++ CategoricalLogic.lean
@@ -1,5 +1,6 @@
-module
 
-public import CategoricalLogic.Profunctor.Basic
-public import CategoricalLogic.Profunctor.Cells
-public import CategoricalLogic.Profunctor.Product
+import CategoricalLogic.Profunctor.Basic
+import CategoricalLogic.Profunctor.Cells
+import CategoricalLogic.Profunctor.Product
+
+import CategoricalLogic.VirtualDoubleCategory.QuiverSpan
